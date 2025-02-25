2df90220ca553d57d30af543571aa29efa3db161
ie0428 <280189670@qq.com>
2025-02-25 22:13:46 +0800

    套餐管理业务功能开发

--- sky-server/src/main/java/com/sky/mapper/setmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/setmealDishMapper.java
@@ -4,8 +4,10 @@ import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.entity.SetmealDish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -13,7 +15,30 @@ import java.util.List;
 
 @Mapper
 public interface setmealDishMapper {
+
+    /**
+     * 批量保存套餐和菜品的关联关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
+
     //根据id查询菜品
 
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据套餐id删除套餐和菜品的关联关系
+     * @param setmealId
+     */
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
+    void deleteBySetmealId(Long setmealId);
+
+
+    /**
+     * 根据套餐id查询套餐和菜品的关联关系
+     * @param setmealId
+     * @return
+     */
+    @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
+    List<SetmealDish> getBySetmealId(Long setmealId);
 }
