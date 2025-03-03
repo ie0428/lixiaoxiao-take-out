5e995e2df3eff08ec4f25cf36559d80a544b20f8
ie0428 <280189670@qq.com>
2025-03-03 14:12:34 +0800

    订单管理业务功能开发

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,53 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    //处理支付超时订单
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void processTimeOutOrder() {
+        log.info("处理支付超时订单:{}", new Date());
+        LocalDateTime time = LocalDateTime.now().minusMinutes(-15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrdertimeLT(Orders.PENDING_PAYMENT, time);
+
+        if (ordersList != null && !ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+            
+        }
+    }
+
+    //处理派送中的订单状态
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void processDeliveryOrder() {
+        log.info("处理派送中的订单状态:{}", new Date());
+        LocalDateTime time = LocalDateTime.now().minusMinutes(-60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrdertimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+
+        if (ordersList != null && !ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
